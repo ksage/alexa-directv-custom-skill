9c4cd9eb6ab97d9ab4a40d38b75c96c624a54f36
Kevin Sage <pacosage@gmail.com>
2018-01-10 23:56:38 -0800

    Add additional TV channels
    Add lots more channel definitions and aliases for local channels

--- directv-node.js
+++ directv-node.js
@@ -143,12 +143,6 @@ exports.handler = (event, context) => {
                 case "channel":
                     path = '/tv/getTuned';
                     break;
-                case "CNN":
-                    path = '/tv/tune?major=202';
-                    break;
-                case "ABC family":
-                    path = '/tv/tune?major=311';
-                    break;
                 case "AMC":
                     path = '/tv/tune?major=254';
                     break;
@@ -158,20 +152,56 @@ exports.handler = (event, context) => {
                 case "animal planet":
                     path = '/tv/tune?major=282';
                     break;
+                case "audience":
+                    path = '/tv/tune?major=239';
+                    break;
+                case "BBC america":
+                    path = '/tv/tune?major=264';
+                    break;
+                case "BBC world news":
+                    path = '/tv/tune?major=346';
+                    break;
                 case "BET":
                     path = '/tv/tune?major=329';
                     break;
+                case "Bloomberg":
+                    path = '/tv/tune?major=353';
+                    break;
                 case "bravo":
                     path = '/tv/tune?major=237';
+                    break;
+                case "CBS Sports Network":
+                    path = '/tv/tune?major=221';
+                    break;
+                case "CMT":
+                    path = '/tv/tune?major=327';
+                    break;
+                case "CNBC":
+                    path = '/tv/tune?major=355';
+                    break;
+                case "CNN":
+                    path = '/tv/tune?major=202';
                     break;
                 case "comedy central":
                     path = '/tv/tune?major=249';
                     break;
                 case "C span":
-                    path = '/tv/tune?major=250';
+                    path = '/tv/tune?major=350';
+                    break;
+                case "C span 2":
+                    path = '/tv/tune?major=351';
                     break;
                 case "cartoon network":
                     path = '/tv/tune?major=296';
+                    break;
+                case "cartoon network west":
+                    path = '/tv/tune?major=297';
+                    break;
+                case "cooking channel":
+                    path = '/tv/tune?major=232';
+                    break;
+                case "DIY network":
+                    path = '/tv/tune?major=230';
                     break;
                 case "discovery":
                     path = '/tv/tune?major=278';
@@ -179,35 +209,145 @@ exports.handler = (event, context) => {
                 case "Disney channel":
                     path = '/tv/tune?major=290';
                     break;
+                case "Disney channel west":
+                    path = '/tv/tune?major=291';
+                    break;
                 case "Disney junior":
                     path = '/tv/tune?major=289';
                     break;
                 case "Disney XD":
                     path = '/tv/tune?major=292';
                     break;
+                case "ESPN":
+                    path = '/tv/tune?major=206';
+                    break;
+                case "ESPN 2":
+                    path = '/tv/tune?major=209';
+                    break;
+                case "ESPN news":
+                    path = '/tv/tune?major=207';
+                    break;
+                case "ESPN U":
+                    path = '/tv/tune?major=208';
+                    break;
+                case "EWTN":
+                    path = '/tv/tune?major=370';
+                    break;
+                case "Fox sports 1":
+                    path = '/tv/tune?major=219';
+                    break;
+                case "Fox sports 2":
+                    path = '/tv/tune?major=618';
+                    break;
                 case "food network":
                     path = '/tv/tune?major=231';
-                    break;
-                case "fox":
-                    path = '/tv/tune?major=360';
                     break;
                 case "FX":
                     path = '/tv/tune?major=248';
                     break;
+                case "FXM":
+                    path = '/tv/tune?major=258';
+                    break;
+                case "FXX":
+                    path = '/tv/tune?major=259';
+                    break;
+                case "Fox business":
+                    path = '/tv/tune?major=359';
+                    break;
+                case "Fox news":
+                    path = '/tv/tune?major=360';
+                    break;
+                case "free form":
+                    path = '/tv/tune?major=311';
+                    break;
+                case "golf channel":
+                    path = '/tv/tune?major=218';
+                    break;
+                case "great american country":
+                    path = '/tv/tune?major=326';
+                    break;
                 case "HGTV":
                     path = '/tv/tune?major=229';
+                    break;
+                case "hallmark channel":
+                    path = '/tv/tune?major=312';
+                    break;
+                case "headline news":
+                    path = '/tv/tune?major=204';
                     break;
                 case "history channel":
                     path = '/tv/tune?major=269';
                     break;
+                case "ion":
+                    path = '/tv/tune?major=305';
+                    break;
+                case "ion west":
+                    path = '/tv/tune?major=306';
+                    break;
+                case "fox":
+                case "KTVU":
+                    path = '/tv/tune?major=2';
+                    break;
+                case "KRON":
+                    path = '/tv/tune?major=4';
+                    break;
+                case "CBS":
+                case "KPIX":
+                    path = '/tv/tune?major=5';
+                    break;
+                case "ABC":
+                case "KGO":
+                    path = '/tv/tune?major=7';
+                    break;
+                case "NBC":
+                case "KNTV":
+                    path = '/tv/tune?major=11';
+                    break;
+                case "KOFY":
+                    path = '/tv/tune?major=20';
+                    break;
+                case "KTSF":
+                    path = '/tv/tune?major=26';
+                    break;
+                case "KTVU plus":
+                case "KICU":
+                    path = '/tv/tune?major=36';
+                    break;
+                case "the CW":
+                case "KBCW":
+                    path = '/tv/tune?major=44';
+                    break;
+                case "PBS":
+                case "KQED":
+                    path = '/tv/tune?major=9';
+                    break;
+                case "KRCB":
+                    path = '/tv/tune?major=22';
+                    break;
+                case "KQEH":
+                    path = '/tv/tune?major=54';
+                    break;
+                case "ion local":
+                case "KKPX":
+                    path = '/tv/tune?major=65';
+                    break;
                 case "lifetime":
                     path = '/tv/tune?major=252';
+                    break;
+                case "lifetime movie network":
+                    path = '/tv/tune?major=253';
                     break;
                 case "MSNBC":
                     path = '/tv/tune?major=356';
                     break;
+                case "MLB network":
+                    path = '/tv/tune?major=213';
+                    break;
                 case "MTV":
                     path = '/tv/tune?major=331';
+                    break;
+                case "MTV classic":
+                    path = '/tv/tune?major=336';
                     break;
                 case "MTV2":
                     path = '/tv/tune?major=333';
@@ -215,8 +355,59 @@ exports.handler = (event, context) => {
                 case "national geographic":
                     path = '/tv/tune?major=276';
                     break;
+                case "NBA TV":
+                    path = '/tv/tune?major=216';
+                    break;
+                case "NBC sports network":
+                    path = '/tv/tune?major=220';
+                    break;
+                case "NBC sports california":
+                    path = '/tv/tune?major=698';
+                    break;
+                case "NBC sports bay area":
+                    path = '/tv/tune?major=696';
+                    break;
+                case "NFL network":
+                    path = '/tv/tune?major=212';
+                    break;
+                case "NHL network":
+                    path = '/tv/tune?major=215';
+                    break;
+                case "nat geo wild":
+                    path = '/tv/tune?major=283';
+                    break;
+                case "national geographic":
+                    path = '/tv/tune?major=276';
+                    break;
                 case "nick junior":
                     path = '/tv/tune?major=301';
+                    break;
+                case "nickelodeon":
+                    path = '/tv/tune?major=299';
+                    break;
+                case "nickelodeon west":
+                    path = '/tv/tune?major=300';
+                    break;
+                case "nick toons":
+                    path = '/tv/tune?major=302';
+                    break;
+                case "teen nick":
+                    path = '/tv/tune?major=303';
+                    break;
+                case "own":
+                    path = '/tv/tune?major=279';
+                    break;
+                case "olympic channel":
+                    path = '/tv/tune?major=624';
+                    break;
+                case "oxygen":
+                    path = '/tv/tune?major=251';
+                    break;
+                case "pop":
+                    path = '/tv/tune?major=273';
+                    break;
+                case "SEC network":
+                    path = '/tv/tune?major=611';
                     break;
                 case "spike":
                     path = '/tv/tune?major=241';
@@ -227,20 +418,38 @@ exports.handler = (event, context) => {
                 case "TBS":
                     path = '/tv/tune?major=247';
                     break;
+                case "TCM":
+                    path = '/tv/tune?major=256';
+                    break;
+                case "TLC":
+                    path = '/tv/tune?major=280';
+                    break;
                 case "TNT":
                     path = '/tv/tune?major=245';
+                    break;
+                case "TV land":
+                    path = '/tv/tune?major=304';
+                    break;
+                case "tennis channel":
+                    path = '/tv/tune?major=217';
+                    break;
+                case "the weather channel":
+                    path = '/tv/tune?major=362';
+                    break;
+                case "travel channel":
+                    path = '/tv/tune?major=277';
                     break;
                 case "true TV":
                     path = '/tv/tune?major=246';
                     break;
+                case "USA":
+                    path = '/tv/tune?major=242';
+                    break;
                 case "VH1":
                     path = '/tv/tune?major=335';
                     break;
-                case "ESPN":
-                    path = '/tv/tune?major=206';
-                    break;
-                case "ESPN2":
-                    path = '/tv/tune?major=209';
+                case "WGN":
+                    path = '/tv/tune?major=307';
                     break;
                 }
                 if (path == "/tv/getTuned") {
